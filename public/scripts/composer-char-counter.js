c99bdeaf8dc869c9c0aa4e3b042bfac366dc0fb2
vagrant <j_khabra11@hotmail.com>
2020-04-21 21:08:34 +0000

    updated character counter to include color changes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,11 @@
 $(document).ready(function() {
-  //console.log("works great!");
 $("#tweet-text").keyup(function(){
   let max = 140;
   let amountOfChars = $(this).val().length;
-  if(amountOfChars <= max){
     let counter = max - amountOfChars;
     $(this).parent().find(".counter").val(counter);
+  if (counter < 0) {
+    $(this).parent().find('.counter').val(counter).css('color', '#FF0000')
   }
 });
 });
-// #tweet-text = text area
-// .counter = the  counter 
-// btn = button
-// <textarea name="text" id="tweet-text"></textarea>
-// <button type="submit">Tweet</button>
-// <output name="counter" class="counter" for="tweet-text">140</output>
-
-// $("#btn").on("click", () => {
-//   console.log(this);
-// });
